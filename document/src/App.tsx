a240f675bd2a4306829295311628a549c734ceda
Kotyarka <varenik.kotyarka@gmail.com>
2025-05-07 15:16:48 +0300

    Add files via upload

--- document/src/App.tsx
+++ document/src/App.tsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-import DocumentsPage from './pages/DocumentsPage';
 import { Layout } from 'antd';
+import DocumentsPage from './pages/DocumentsPage';
+import LoginPage from './pages/LoginPage';
 
 const { Header, Content } = Layout;
 
 function App() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Header style={{ color: 'white' }}>Система документооборота</Header>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Header style={{ color: 'white', fontSize: '18px' }}>
+          Система документооборота
+        </Header>
         <Content style={{ padding: '24px' }}>
           <Routes>
             <Route path="/" element={<LoginPage />} />
