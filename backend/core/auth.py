aed7877f8d965fa2a8c0277ac7fa8c7d86d7baa8
tartarus8 <griartemevg@gmail.com>
2025-05-13 14:35:29 +0300

    auth comments

--- backend/core/auth.py
+++ backend/core/auth.py
@@ -6,12 +6,15 @@ from core.config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+# hash_password(user_data.password)
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
+# verify_password(form_data.password, user.hashed_password)
 def verify_password(plain: str, hashed: str) -> bool:
     return pwd_context.verify(plain, hashed)
 
+# create_access_token({"sub": user.username})
 def create_access_token(data: dict, expires_delta: timedelta = timedelta(hours=1)) -> str:
     to_encode = data.copy()
     expire = datetime.utcnow() + expires_delta
